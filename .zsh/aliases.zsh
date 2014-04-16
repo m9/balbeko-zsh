5b45a552df9bb66758a7736b97ca3ef542f1420b
Sergey Balbeko <sergey@balbeko.com>
2014-04-16 12:16:53 +0000

    Initial commit

---
+++ .zsh/aliases.zsh
@@ -0,0 +1,17 @@
+# Colorize output, add file type indicator, and put sizes in human readable format
+alias ls='ls -GFh'
+
+# Same as above, but in long listing format
+alias ll='ls -GFhl'
+
+alias ri='ri -Tf ansi'
+
+alias mysql='mysql -u root'
+alias mysqladmin='mysqladmin -u root'
+
+alias be='bundle exec'
+alias bx='bundle exec'
+
+alias gentags='ctags . && rm .tags;mv tags .tags'
+alias rake='noglob rake'
+alias bower='noglob bower'
---
+++ .zshrc
@@ -0,0 +1,14 @@
+source ~/.zsh/colors.zsh
+source ~/.zsh/setopt.zsh
+source ~/.zsh/exports.zsh
+source ~/.zsh/prompt.zsh
+source ~/.zsh/completion.zsh
+source ~/.zsh/aliases.zsh
+source ~/.zsh/bindkeys.zsh
+source ~/.zsh/functions.zsh
+source ~/.zsh/history.zsh
+source ~/.zsh/zsh_hooks.zsh
+source ~/.zsh/hitch.zsh
+
+### Added by the Heroku Toolbelt
+export PATH="/usr/local/heroku/bin:$PATH"
